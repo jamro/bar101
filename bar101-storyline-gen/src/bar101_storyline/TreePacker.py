d4c3fb1c3715a442b712dceace675ce25b50235e
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-04-21 00:06:39 +0200

    clean up texts from utf chars

--- bar101-storyline-gen/src/bar101_storyline/TreePacker.py
+++ bar101-storyline-gen/src/bar101_storyline/TreePacker.py
@@ -9,6 +9,39 @@ class TreePacker:
             raise FileNotFoundError(f"File not found at {file_path}")
         with open(file_path, "r") as f:
             return json.load(f)
+        
+    def _clean(self, data):
+        if isinstance(data, str):
+            data = data.replace("\u002d", "-")
+            data = data.replace("\u2011", "-")
+            data = data.replace("\u2013", "-")
+            data = data.replace("\u2014", " - ")
+            data = data.replace("\u2019", "'")
+            data = data.replace("\u2026", "...")
+            data = data.replace("\u2018", "'")
+            data = data.replace("\u201c", "'")
+            data = data.replace("\u201d", "'")
+            data = data.replace("\u0019", "'")
+            data = data.replace("\u00e9", "e")
+            data = data.replace("\u0014", " ")
+            data = data.replace("\u202f", " ")
+            data = data.replace("\"", "'")
+            if data.startswith("'") and data.endswith("'"):
+                data = data[1:-1]
+
+            return data
+        
+        if isinstance(data, list):
+            return [self._clean(item) for item in data]
+        
+        if isinstance(data, dict):
+            cleaned_data = {}
+            for key, value in data.items():
+                cleaned_value = self._clean(value)
+                cleaned_data[key] = cleaned_value
+            return cleaned_data
+        
+        return data
    
     def pack_node(self, path):
         characters = self._read_file(path, "characters.json")
@@ -30,11 +63,13 @@ class TreePacker:
                 "political_preference": characters[character]["political_preference"],
             }
 
-        return {
+        raw_result = {
             "news": self._read_file(path, "news.json"),
             "visitors": visitors,
             "character_stats": character_stats,
             "chats": visitors_chat,
             
         }
+
+        return self._clean(raw_result)
         
