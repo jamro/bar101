804f70a35ff2613c248813e0f9a72b65fe815566
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-04-23 20:59:45 +0200

    add voice bank

---
+++ bar101-voiceover/src/bar101_voiceover/lib/voice_bank.py
@@ -0,0 +1,83 @@
+voice_bank = {
+    "news_official": {
+        "id": "news_official",
+        "speker": "p317",
+        "samples": [
+            "Council Affirms Clarity",
+            "The Council has released new details on the recent BCI disruption, promising a transparent review and stability in services.",
+            "Stenograd's trusted processes ensure accountability and continuous system improvement for all residents."
+        ]
+    },
+    "news_underground": {
+        "id": "news_underground",
+        "speker": "p248",
+        "samples": [
+            "Halden's Loop: City Hacked",
+            "Speakers blast Halden's coded message - crowds gather, BCI glitches out, fear and hope on every face.",
+            "They call it unity, but scores spike and drop with every ritual; the system is showing cracks, and people are searching for answers the Council won't give."
+        ]
+    },
+    "aradan": {
+        "id": "aradan",
+        "speker": "p267",
+        "samples": [
+            "What can I get you?",
+            "Long walk to get here?",
+            "Giving people the facts is the only way they learn to stand up straight.",
+        ]
+    },
+    "olintz": {
+        "id": "olintz",
+        "speker": "p308",
+        "samples": [
+            "Thanks for the drink. I should go. Early shift tomorrow.",
+            "Unless this is your way of testing me, I'll still take the daiquiri.",
+            "Long day. System glitches at home made everything awkward."
+        ]
+    },
+    "npetrak": {
+        "id": "npetrak",
+        "speker": "p340",
+        "samples": [
+            "Old fashioned - and maybe a word off the record.",
+            "Made it, not without obstacles - they're changing access rules again.",
+            "Hard getting ribbon now, even harder with basement codes changing."
+        ] 
+    },
+    "dtomenko": {
+        "id": "dtomenko",
+        "speker": "p283",
+        "samples": [
+            "You know what I need before I ask. Useful.",
+            "Yeah, flying's still the best way to zone out.",
+            "It's rare anyone admits there's weight here. Most just look away, pretend this is normal."
+        ]
+    },
+    "lkova": {
+        "id": "lkova",
+        "speker": "p287",
+        "samples": [
+            "Barely, with my sanity intact.",
+            "System's constant monitoring used to stop at surface level, now it's everywhere.",
+            "I've seen propaganda floods before, but this? Feels like the system's holding its breath and hoping we all play along.",
+        ]
+    },
+    "shalek": {
+        "id": "shalek",
+        "speker": "p228",
+        "samples": [
+            "Barter's back - people trade, official credits don't always work.",
+            "Odd, but in chaos, these books matter more than ever.",
+            "Makes me wonder if words themselves threaten the system, or just their persistence.",
+        ]
+    },
+    "rmiskovic": {
+        "id": "rmiskovic",
+        "speker": "p335",
+        "samples": [
+            "Tried to capture that, but got flagged moving zones this week.",
+            "Still going, but film's getting harder to come by.",
+            "Transit's been a pain lately. North side was less of a hassle than usual, for a change.",
+        ]
+    }
+}
--- bar101-voiceover/src/bar101_voiceover/samples.py
+++ bar101-voiceover/src/bar101_voiceover/samples.py
@@ -1,16 +1,28 @@
 from TTS.api import TTS
 import os
+from lib import voice_bank
 
 tts = TTS(model_name="tts_models/en/vctk/vits", progress_bar=True)
 
 for speaker in tts.speakers:
-    #remove non-alphabetic or non-numeric characters from speaker name
     speaker_id = ''.join(filter(lambda x: x.isalnum(), speaker))
     print(f"Generating voice for speaker: {speaker_id}")
-    file_path = os.path.join(os.path.dirname(__file__), "..", "..", "voiceovers", "samples", f"{speaker_id}.wav")
+    file_path = os.path.join(os.path.dirname(__file__), "..", "..", "voiceovers", "samples", "voices", f"{speaker_id}.wav")
+    if not os.path.exists(file_path):
+        tts.tts_to_file(
+            text="I'm telling you, this city is changing.",
+            speaker=speaker,
+            file_path=file_path,
+        )
 
-    tts.tts_to_file(
-        text="I'm telling you, this city is changing.",
-        speaker=speaker,
-        file_path=file_path
-    )
+
+for voice in voice_bank.values():
+    for index, sample in enumerate(voice["samples"]):
+        speaker_id = voice["id"]
+        file_path = os.path.join(os.path.dirname(__file__), "..", "..", "voiceovers", "samples", "characters", f"{speaker_id}_{index+1}.wav")
+        if not os.path.exists(file_path):
+            tts.tts_to_file(
+                text=sample,
+                speaker=voice["speker"],
+                file_path=file_path,
+            )
