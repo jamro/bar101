5d584c689fc6da83f0ca8de3b03493f4faacb5bb
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-04-24 21:42:12 +0200

    fix showing job title

--- bar101-game/src/components/CustomerPreview.jsx
+++ bar101-game/src/components/CustomerPreview.jsx
@@ -6,17 +6,19 @@ export default function CustomerPreview({ customer, drink, children }) {
   const { 
     id, 
     name, 
-    jobTitle, 
+    job_title: jobTitle, 
     trust, 
     bci_score: bciScore, 
     political_preference: politicalPreference 
   } = customer;
+  console.log(customer)
   return (
     <div className={styles.customerContainer}>
       <div className={styles.customerHeader}>
         <h2 className='mb-0'>{name}</h2>
         <p>
           {jobTitle}
+          <br/>
           <strong>BCI Score:</strong> {Math.round(bciScore)}%
           <br/>
           <strong>Political Preference:</strong> {politicalPreference}
