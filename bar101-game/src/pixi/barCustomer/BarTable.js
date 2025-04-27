ce099f88ce232317a4a3c3c57562fc7afb1574b8
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-04-27 18:44:37 +0200

    optimise graphics

--- bar101-game/src/pixi/barCustomer/BarTable.js
+++ bar101-game/src/pixi/barCustomer/BarTable.js
@@ -1,6 +1,17 @@
 import * as PIXI from 'pixi.js';
 import GameAssets from '../GameAssets';
 
+const assetsPosition = {
+  'img/rocks.png': { x: 417, y: 492 },
+  'img/coupe.png': { x: 409, y: 444 },
+  'img/dtomenko.png': { x: 301, y: 94 },
+  'img/olintz.png': { x: 262, y: 82 },
+  'img/lkova.png': { x: 228, y: 57 },
+  'img/npetrak.png': { x: 222, y: 82 },
+  'img/rmiskovic.png': { x: 273, y: 92 },
+  'img/shalek.png': { x: 261, y: 83 },
+};
+
 export default class BarTable extends PIXI.Container {
 
   constructor() {
@@ -17,6 +28,8 @@ export default class BarTable extends PIXI.Container {
     }
     if(customer) {
       this._customerSprite = new PIXI.Sprite(GameAssets.assets[`img/${customer.id}.png`]);
+      this._customerSprite.x = assetsPosition[`img/${customer.id}.png`].x;
+      this._customerSprite.y = assetsPosition[`img/${customer.id}.png`].y;
       this.addChild(this._customerSprite);
     }
   }
@@ -27,6 +40,8 @@ export default class BarTable extends PIXI.Container {
     }
     if(drink) {
       this._drinkSprite = new PIXI.Sprite(GameAssets.assets[`img/${drink.glass}.png`]);
+      this._drinkSprite.x = assetsPosition[`img/${drink.glass}.png`].x;
+      this._drinkSprite.y = assetsPosition[`img/${drink.glass}.png`].y;
       this.addChild(this._drinkSprite);
     }
   }
