6c0f3d3b9d68a33f665f400f4682281b15ef852c
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-06-01 13:01:44 +0200

    add cdn to terraform

--- bar101-infra/terraform/main.tf
+++ bar101-infra/terraform/main.tf
@@ -84,3 +84,60 @@ resource "aws_s3_bucket_policy" "s3_bar101_app_policy" {
 
   depends_on = [aws_s3_bucket_public_access_block.s3_bar101_app_pab]
 }
+
+# S3 bucket for CDN
+resource "aws_s3_bucket" "s3_cdn_bar101" {
+  bucket = "cdn-bar101.jmrlab.com"
+}
+
+resource "aws_s3_bucket_website_configuration" "s3_cdn_bar101_website" {
+  bucket = aws_s3_bucket.s3_cdn_bar101.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "s3_cdn_bar101_cors" {
+  bucket = aws_s3_bucket.s3_cdn_bar101.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = [
+      "http://localhost:3000",
+      "http://192.168.68.58:3000",
+      "https://bar101.jmrlab.com"
+    ]
+    expose_headers  = ["Content-Length"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_cdn_bar101_pab" {
+  bucket = aws_s3_bucket.s3_cdn_bar101.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "s3_cdn_bar101_policy" {
+  bucket = aws_s3_bucket.s3_cdn_bar101.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::cdn-bar101.jmrlab.com/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.s3_cdn_bar101_pab]
+}
