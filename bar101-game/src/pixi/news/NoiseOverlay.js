d97766f2f7325982722e0e08e10f441bd1b1e83d
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-06-03 09:11:08 +0200

    reduce TV noise

--- bar101-game/src/pixi/news/NoiseOverlay.js
+++ bar101-game/src/pixi/news/NoiseOverlay.js
@@ -7,16 +7,38 @@ export default class NoiseOverlay extends PIXI.Container {
     this._canvas = new PIXI.Graphics()
     this.addChild(this._canvas)
     
-
     setInterval(() => this.redraw(), 30)
+
+    this._phase = 0
+    this._phaseTimer = 0
   }
 
   redraw() {
     const width = 1020;
     const height = 670
     this._canvas.clear()
-    if(Math.random() < 0.1) {
 
+
+    this._phaseTimer--;
+
+    console.log(this._phase, this._phaseTimer)
+
+    if(this._phaseTimer < 0) {
+      this._phase = (this._phase + 1) % 2
+
+      if(this._phase === 0) { // no noise
+        this._phaseTimer = 100 + Math.random() * 100
+      } else { // noise
+        this._phaseTimer = 10 + Math.random() * 50
+      }
+    }
+
+    if(this._phase === 0) {
+      return;
+    }
+
+
+    if(Math.random() < 0.2) { // draw noisy lines
       const lineCount = Math.floor(Math.random() * Math.random() * 30) + 1
       for(let i = 0; i < lineCount; i++) {
         const y = Math.random() * height
@@ -27,11 +49,9 @@ export default class NoiseOverlay extends PIXI.Container {
           1 + Math.floor(Math.random() * 10)
         ).fill(Math.random() > 0.5 ? 0x000000 : 0x000000)
       }
-
     }
 
-
-    if(Math.random() < 0.05) {
+    if(Math.random() < 0.1) { // draw noisy rectangles
       const rectCount = Math.floor(Math.random() * Math.random() * 3) + 1
       for(let i = 0; i < rectCount; i++) {
         const w = Math.random() * width * 0.2 + width * 0.05
